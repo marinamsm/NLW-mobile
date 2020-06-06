9a783cbb2bd6b33b0dbb2da0ac41b075cdaa25dc
marina <marinamsm@live.com>
2020-06-06 15:00:25 -0300

    Homepage is ready

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { StyleSheet } from 'react-native';
+import { Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+
+import Home from './src/pages/Home/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium
+
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-    </View>
+    <Home />
   );
 }
 
