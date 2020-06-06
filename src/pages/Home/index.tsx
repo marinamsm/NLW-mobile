623fbe78238d1f5cf4cb39e9f01edeabb7cc3871
marina <marinamsm@live.com>
2020-06-06 14:07:31 -0300

    Mispelled word

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,10 @@
-import react from 'react';
+import React from 'react';
 import { View } from 'react-native';
 
+const Home = () => {
+    return (
+        <View />
+    )
+}
+
+export default Home;
